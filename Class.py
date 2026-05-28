add5ddb702e3c2b06535944dc3eecc48af4be283
sankar <sankarnarayana910@gmail.com>
2026-05-28 11:23:57 +0530

    updated

---
+++ Class.py
@@ -0,0 +1,18 @@
+# def is_anagram(a, b):
+
+#     if len(a) != len(b):
+#         return "Not Anagram"
+
+#     if sorted(a) == sorted(b):
+#         return "Anagram"
+#     else:
+#         return "Not Anagram"
+
+# print(is_anagram("race", "care"))
+
+
+
+
+
+
+s1, s2 = "listen", "silent"; print(sorted(s1.care()) == sorted(s2.race()))
