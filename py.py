bcb5cd1ad2fc991e3abdbd0d9bc0fc678a8faeb7
sankar <sankarnarayana910@gmail.com>
2026-06-19 17:35:01 +0530

    up

---
+++ py.py
@@ -0,0 +1,43 @@
+# print("hello world")
+
+
+
+# a = input("enter the name: ")
+# print(a)
+# b =a[::-1]
+# print(b)
+
+
+
+# if 1 > 2:
+#     print("1 is grater than 2")
+
+# elif 1< 2:
+#     print("1 is less than 2 ")
+    
+    
+    
+# num1 =int(input("enter the number: "))
+# num2 =int(input("enter the number: "))
+# if num1 > num2:
+#     print("number 1 greater than num2")
+# elif num2 > num1:
+#     print("number 2 is greater than num1")
+
+# else:
+#     print("invalid number are both number are same")
+
+
+
+# i = 10
+# while i < 20:
+#     print(i)
+#     i+1
+
+
+
+for i in range(1,9):
+    if i == 3:
+        continue
+    print(i)
+        
